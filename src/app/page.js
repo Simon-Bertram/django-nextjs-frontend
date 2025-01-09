63102a48fbecfb5a90f565cd7ca09b102120c7be
Simon-Bertram <simonbertram@hotmail.com>
2025-01-09 14:57:57 +0000

    Use SWR to fetch data from django API

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,34 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Home() {
-  async function getDjangoAPIData() {
-    const response = await fetch("http://127.0.0.1:8001/api/hello");
-    const data = await response.json();
-    console.log(data);
-  }
+  // GET requests
+  const { data, error, isLoading } = useSWR(
+    "http://127.0.0.1:8001/api/hello",
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  // const [data, setData] = useState({});
+  // async function getDjangoAPIData() {
+  //   const response = await fetch("http://127.0.0.1:8001/api/hello");
+  //   const responseData = await response.json();
+  //   // console.log(data);
+  //   setData(responseData);
+  // }
 
-  async function handleClick() {
-    await getDjangoAPIData();
-  }
+  // async function handleClick() {
+  //   await getDjangoAPIData();
+  // }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <button onClick={handleClick}>Lookup data</button>
+        <div>{JSON.stringify(data)}</div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
